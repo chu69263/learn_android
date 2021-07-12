4ce4e4c50e34d10d89ea7155661af17b031a11af
chu69263 <hhb635471201@gmail.com>
2021-07-12 18:36:50 +0800

    1

---
+++ app/src/main/java/com/waibibabo/learn/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.waibibabo.learn.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.waibibabo.learn_android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
