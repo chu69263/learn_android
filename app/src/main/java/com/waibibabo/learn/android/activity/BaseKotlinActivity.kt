1dc8b6fdcf61c0876120072556de52e55ec383fa
chu69263 <hhb635471201@gmail.com>
2021-07-14 19:05:05 +0800

    1

--- app/src/main/java/com/waibibabo/learn/android/activity/BaseKotlinActivity.kt
+++ app/src/main/java/com/waibibabo/learn/android/activity/BaseKotlinActivity.kt
@@ -1,5 +1,6 @@
 package com.waibibabo.learn.android.activity
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.waibibabo.learn.android.annotation.Page
 
@@ -9,4 +10,40 @@ import com.waibibabo.learn.android.annotation.Page
  */
 @Page(name = "kotlin基础")
 class BaseKotlinActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "BaseKotlinActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        var i: Int = 1
+        var str: String = if (i == 1) {
+            "hehe"
+        } else {
+            "heihei"
+        }
+        str = when (i) {
+            1 -> {
+                println("这是1")
+                "when"
+            }
+            else -> "when2"
+        }
+        i = add(1, 2)
+        var innerFunc: (String, Int) -> Int = { q, _ -> q.length }
+        funArg(TAG) { str ->
+            println("callback $str")
+        }
+    }
+
+    private fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    private fun add2(a: Int, b: Int): Int = a + b
+
+    private fun funArg(str: String, success: (String) -> Unit) {
+        success(str)
+    }
 }
